868003eabadf39d5ef50cd32ee105acb1c5c2904
Varun Joshi <varun.joshi@cern.ch>
2017-06-28 11:57:20 +0200

    Refine models and use SQLAlchemy-Utc

--- urlshortener/models.py
+++ urlshortener/models.py
@@ -1,20 +1,25 @@
+import datetime
 from uuid import uuid4
 
 from sqlalchemy.dialects.postgresql import JSONB, UUID
-from urlshortener.core.db import db
+from sqlalchemy_utc import UtcDateTime
+
+from urlshortener import db
 
 
 class Token(db.Model):
     __tablename__ = 'tokens'
 
-    id = db.Column(UUID(as_uuid=True),
-                   primary_key=True,
-                   default=lambda: str(uuid4()))
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(UUID,
+                     unique=True,
+                     index=True,
+                     default=lambda: str(uuid4()))
     name = db.Column(db.Text, nullable=False, unique=True)
     is_admin = db.Column(db.Boolean, nullable=False, default=False)
     is_blocked = db.Column(db.Boolean, nullable=False, default=False)
-    token_uses = db.Column(db.Integer, nullable=False)
-    last_access = db.Column(db.DateTime, nullable=False)
+    token_uses = db.Column(db.Integer, nullable=False, default=0)
+    last_access = db.Column(UtcDateTime, nullable=False, default=datetime.utcnow)
     creator = db.Column(db.ForeignKey('tokens.id'))
     urls = db.relationship('URL', backref='token')
 
@@ -24,6 +29,5 @@ class URL(db.Model):
 
     shorturl = db.Column(db.String(5), primary_key=True)
     url = db.Column(db.Text, unique=False)
-    token_id = db.Column(UUID(as_uuid=True),
-                         db.ForeignKey('tokens.id'))
+    token_id = db.Column(db.ForeignKey('tokens.id'))
     urldata = db.Column(JSONB)
