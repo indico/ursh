95fc1c3b2d8f455824fc9af50d89e9495bd65242
Varun Joshi <varun.joshi@cern.ch>
2017-06-29 15:34:42 +0200

    Move things around in models.py

--- urlshortener/models.py
+++ urlshortener/models.py
@@ -17,10 +17,11 @@ class Token(db.Model):
     is_blocked = db.Column(db.Boolean, nullable=False, default=False)
     token_uses = db.Column(db.Integer, nullable=False, default=0)
     last_access = db.Column(UtcDateTime, nullable=False, default=lambda: datetime.now(tz=timezone.utc))
+
     urls = db.relationship('URL', back_populates='token')
 
     def __repr__(self):
-        return f"<Token({self.id}, {self.api_key}): {self.name}>"
+        return f'<Token({self.id}, {self.api_key}): {self.name}>'
 
 
 class URL(db.Model):
@@ -30,8 +31,9 @@ class URL(db.Model):
     shortcut = db.Column(db.String, unique=True, index=True)
     url = db.Column(db.String, nullable=False)
     token_id = db.Column(db.ForeignKey('tokens.id'), nullable=False)
-    token = db.relationship('Token', back_populates='urls')
     custom_data = db.Column(JSONB, default={}, nullable=False)
 
+    token = db.relationship('Token', back_populates='urls')
+
     def __repr__(self):
-        return f"<URL({self.id}, {self.shortcut}): {self.url}>"
+        return f'<URL({self.id}, {self.shortcut}): {self.url}>'
