1d16b2a3f350c0b7a2a3310853252c26b1c009b7
Varun Joshi <varun.joshi@cern.ch>
2017-06-29 15:27:16 +0200

    Change models, install package in .travis.yml

--- urlshortener/models.py
+++ urlshortener/models.py
@@ -17,14 +17,21 @@ class Token(db.Model):
     is_blocked = db.Column(db.Boolean, nullable=False, default=False)
     token_uses = db.Column(db.Integer, nullable=False, default=0)
     last_access = db.Column(UtcDateTime, nullable=False, default=lambda: datetime.now(tz=timezone.utc))
-    creator = db.Column(db.ForeignKey('tokens.id'))
-    urls = db.relationship('URL', backref='token')
+    urls = db.relationship('URL', back_populates='token')
+
+    def __repr__(self):
+        return f"<Token({self.id}, {self.api_key}): {self.name}>"
 
 
 class URL(db.Model):
     __tablename__ = 'urls'
 
-    shorturl = db.Column(db.String, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    shortcut = db.Column(db.String, unique=True, index=True)
     url = db.Column(db.String, nullable=False)
     token_id = db.Column(db.ForeignKey('tokens.id'), nullable=False)
+    token = db.relationship('Token', back_populates='urls')
     custom_data = db.Column(JSONB, default={}, nullable=False)
+
+    def __repr__(self):
+        return f"<URL({self.id}, {self.shortcut}): {self.url}>"
