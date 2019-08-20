9c8bf877c815b32aacf161b64a54be2bac515d0f
Adrian Moennich <adrian.moennich@cern.ch>
2019-08-20 12:55:03 +0200

    Add docstring to index endpoint
    otherwise redoc docs are ugly

--- ursh/blueprints/misc.py
+++ ursh/blueprints/misc.py
@@ -6,6 +6,25 @@ bp = Blueprint('misc', __name__)
 
 @bp.route('/')
 def index():
+    """Index URL (no shortcut specified).
+    ---
+    get:
+      tags:
+      - public
+      summary: shows a message or redirects to a configured url
+      responses:
+        200:
+          description: default message if no redirect target is configured
+        302:
+          description: redirect to the configured target url
+    options:
+      tags:
+      - public
+      summary: responds with the allowed HTTP methods
+      responses:
+        200:
+          description: OK
+    """
     if current_app.config['INDEX_REDIRECT']:
         return redirect(current_app.config['INDEX_REDIRECT'])
     return Response('Nothing to see here', content_type='text/plain')
